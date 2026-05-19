dded773a8d9e9ad8dfa75d9defa9ab93169ab81f
musheabdulhakim <musheabdulhakim99@gmail.com>
2026-05-19 11:09:10 -0400

    fix diagnostics cluster route conflict

--- app/Filament/Clusters/Diagnostics/DiagnosticsCluster.php
+++ app/Filament/Clusters/Diagnostics/DiagnosticsCluster.php
@@ -8,5 +8,7 @@ use Filament\Support\Icons\Heroicon;
 
 class DiagnosticsCluster extends Cluster
 {
+    protected static ?string $slug = 'diagnostics-cluster';
+
     protected static string|BackedEnum|null $navigationIcon = Heroicon::OutlinedSquares2x2;
 }
