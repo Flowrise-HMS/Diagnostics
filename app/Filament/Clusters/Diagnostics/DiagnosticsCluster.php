b457364852dcb71cfbc5c4885e40875bca60ac97
musheabdulhakim <musheabdulhakim99@gmail.com>
2026-09-19 19:36:49 +0000

    feat: place the diagnostics cluster in the Patient Care sidebar group

--- app/Filament/Clusters/Diagnostics/DiagnosticsCluster.php
+++ app/Filament/Clusters/Diagnostics/DiagnosticsCluster.php
@@ -5,10 +5,15 @@ namespace Modules\Diagnostics\Filament\Clusters\Diagnostics;
 use BackedEnum;
 use Filament\Clusters\Cluster;
 use Filament\Support\Icons\Heroicon;
+use Modules\Core\Enums\SidebarGroup;
 
 class DiagnosticsCluster extends Cluster
 {
     protected static ?string $slug = 'diagnostics-cluster';
 
-    protected static string|BackedEnum|null $navigationIcon = Heroicon::OutlinedSquares2x2;
+    protected static string|BackedEnum|null $navigationIcon = Heroicon::OutlinedBeaker;
+
+    protected static string|\UnitEnum|null $navigationGroup = SidebarGroup::PatientCare;
+
+    protected static ?int $navigationSort = 50;
 }
